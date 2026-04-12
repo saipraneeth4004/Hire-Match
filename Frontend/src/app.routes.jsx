19d3bdaebf12eced759dbf3811b21ea9098e60a6
saipraneeth4004 <2300031549@kluniversity.in>
2026-04-13 03:52:22 +0530

    frontend login and register done

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router"; 
 import Login from "./features/auth/pages/login";
 import Register from "./features/auth/pages/register";
+import Protected from "./features/auth/components/Protected";
 
 
 export const router = createBrowserRouter([
@@ -12,5 +13,9 @@ export const router = createBrowserRouter([
         path:"/register",
         element:<Register/>
     },
+    {
+        path:"/",
+        element:<Protected><h1>Home Page</h1></Protected>
+    }
 
 ]) 
--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,11 +1,15 @@
-import { createContext,useState } from "react"; 
+import { createContext,useState,useEffect} from "react"; 
+
 
 export const AuthContext = createContext() 
 
 
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(false) 
+    const [loading, setLoading] = useState(true) 
+
+
+ 
 
     return (
         <AuthContext.Provider value={{user,setUser,loading,setLoading}}>
