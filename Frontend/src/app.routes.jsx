26e5d6995c87653f3fb3e71bc5a566f7734bbbab
Saipraneeth <2300031549@kluniversity.in>
2026-05-03 23:57:40 +0530

    Update app.routes.jsx

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,7 +2,7 @@ import {createBrowserRouter} from "react-router";
 import Login from "./features/auth/pages/login";
 import Register from "./features/auth/pages/register";
 import Protected from "./features/auth/components/Protected";
-import Home from "./features/interview/pages/home";
+import Home from "./features/interview/pages/Home";
 import Interview from "./features/interview/pages/interview";
 
 export const router = createBrowserRouter([
